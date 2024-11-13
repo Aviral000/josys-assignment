13559d228d2a275e635cd68edd079df6a18ae189
Aviral000 <avimalik02@gmail.com>
2024-11-13 15:05:48 +0530

    Added more

--- Nov-11/basic.js
+++ Nov-11/basic.js
@@ -2,32 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
 console.log("Original Array: ", arr);
-arr.push(11);
-console.log("Array after push: ", arr);
-arr.pop();
-console.log("Array after pop: ", arr);
-arr.unshift(0);
-console.log("Array after unshift: ", arr);
-arr.shift();
-console.log("Array after shift: ", arr);
-var mapArr = arr.map(function (element, index) {
-    return element + (index + 1);
-});
-console.log("Array after map: ", mapArr);
-var filterArr = arr.filter(function (element) { return element % 2 === 0; });
-console.log("Array after filter: ", filterArr);
-var reduceArr = arr.reduce(function (prev, element) { return prev + element; }, 0);
-console.log("Array after reduce: ", reduceArr);
-arr.splice(5, 5);
-console.log("Array after splice: ", arr);
-var sliceArr = arr.slice(0, 3);
-console.log("Array after slice: ", sliceArr);
-arr.forEach(function (element, index) {
-    setTimeout(function () {
-        console.log(element);
-    }, 250 * (index + 1));
-});
+// arr.push(11);
+// console.log("Array after push: ", arr);
+// arr.pop();
+// console.log("Array after pop: ", arr);
+// arr.unshift(0);
+// console.log("Array after unshift: ", arr);
+// arr.shift();
+// console.log("Array after shift: ", arr);
+// let mapArr: number[] = arr.map((element, index) => {
+//     return element+(index+1);
+// })
+// console.log("Array after map: ", mapArr);
+// let filterArr: number[] = arr.filter((element) => element % 2 === 0);
+// console.log("Array after filter: ", filterArr);
+// let reduceArr: number = arr.reduce((prev, element) => prev + element, 0);
+// console.log("Array after reduce: ", reduceArr);
+// arr.splice(5, 5);
+// console.log("Array after splice: ", arr);
+// const sliceArr: number[] = arr.slice(0, 3);
+// console.log("Array after slice: ", sliceArr);
+// arr.forEach((element, index) => {
+//     setTimeout(() => {
+//         console.log(element);
+//     }, 250*(index+1));
+// })
 var findThree = arr.find(function (element) { return element === 6; });
 console.log("Array after find: ", findThree);
 var findIndexOfThree = arr.findIndex(function (element) { return element === 3; });
 console.log("Array after findIndex: ", findIndexOfThree);
+var includeThree = arr.includes(3);
+console.log("Array after include: ", includeThree);
--- Nov-11/basic.ts
+++ Nov-11/basic.ts
@@ -1,45 +1,99 @@
 let arr: number[] = [1,2,3,4,5,6,7,8,10];
 console.log("Original Array: ", arr);
 
-arr.push(11);
-console.log("Array after push: ", arr);
+// arr.push(11);
+// console.log("Array after push: ", arr);
 
-arr.pop();
-console.log("Array after pop: ", arr);
+// arr.pop();
+// console.log("Array after pop: ", arr);
 
-arr.unshift(0);
-console.log("Array after unshift: ", arr);
+// arr.unshift(0);
+// console.log("Array after unshift: ", arr);
 
-arr.shift();
-console.log("Array after shift: ", arr);
+// arr.shift();
+// console.log("Array after shift: ", arr);
 
-let mapArr: number[] = arr.map((element, index) => {
-    return element+(index+1);
-})
-console.log("Array after map: ", mapArr);
+// let mapArr: number[] = arr.map((element, index) => {
+//     return element+(index+1);
+// })
+// console.log("Array after map: ", mapArr);
 
-let filterArr: number[] = arr.filter((element) => element % 2 === 0);
-console.log("Array after filter: ", filterArr);
+// let filterArr: number[] = arr.filter((element) => element % 2 === 0);
+// console.log("Array after filter: ", filterArr);
 
-let reduceArr: number = arr.reduce((prev, element) => prev + element, 0);
-console.log("Array after reduce: ", reduceArr);
+// let reduceArr: number = arr.reduce((prev, element) => prev + element, 0);
+// console.log("Array after reduce: ", reduceArr);
 
-arr.splice(5, 5);
-console.log("Array after splice: ", arr);
+// arr.splice(5, 5);
+// console.log("Array after splice: ", arr);
 
-const sliceArr: number[] = arr.slice(0, 3);
-console.log("Array after slice: ", sliceArr);
+// const sliceArr: number[] = arr.slice(0, 3);
+// console.log("Array after slice: ", sliceArr);
 
-arr.forEach((element, index) => {
-    setTimeout(() => {
-        console.log(element);
-    }, 250*(index+1));
-})
+// arr.forEach((element, index) => {
+//     setTimeout(() => {
+//         console.log(element);
+//     }, 250*(index+1));
+// })
 
 const findThree: (number | undefined) = arr.find((element) => element === 6);
 console.log("Array after find: ", findThree);
 
 const findIndexOfThree: (number | undefined) = arr.findIndex((element) => element === 3);
 console.log("Array after findIndex: ", findIndexOfThree);
+
+const includeThree: (boolean | undefined) = arr.includes(3);
+console.log("Array after include: ", includeThree);
+
+
+// function processInput(input: string | number | boolean | number[]) {
+//     if (typeof input === "string") {
+//       return `String length: ${input.length}`;
+//     } else if (typeof input === "number") {
+//       return `Rounded number: ${Math.round(input)}`;
+//     } else if (typeof input === "boolean") {
+//       return input ? "Boolean is true" : "Boolean is false";
+//     } else if (Array.isArray(input)) {
+//       const sum = input.reduce((acc, num) => acc + num, 0);
+//       return `Sum of array: ${sum}`;
+//     }
+//     else{
+//       return 'else block'
+//     }
+//   }
+
+//     function processInput(input: string | number | boolean | number[]): void {
+//     if (typeof input === "string") {
+//        console.log(`String length: ${input.length}`);
+//     } else if (typeof input === "number") {
+//       console.log(`Rounded number: ${Math.round(input)}`);
+//     } else if (typeof input === "boolean") {
+//       console.log(input ? "Boolean is true" : "Boolean is false");
+//     } else if (Array.isArray(input)) {
+//       const sum = input.reduce((acc, num) => acc + num, 0);
+//       console.log(`Sum of array: ${sum}`);
+//     }
+//   }
+   
+//   console.log(processInput("Hello TypeScript"));
+//   console.log(processInput(42.89));            
+//   console.log(processInput(true));              
+//   console.log(processInput([1, 2, 3, 4]));
+
+// function processInput(input: number | string): string {
+//     if (typeof input === "number") {
+//         return `Number: ${input * 2}`;
+//     }
+//     else if (typeof input === "string") {
+//         return `String: ${input.toUpperCase()}`;
+//     }
+//     else {
+//         return '';
+//     }
+// }
+
+// console.log(processInput(42));
+// console.log(processInput("hello"));
+// console.log(processInput(null));
 
 export {}
