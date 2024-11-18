47bf4ccd8183faaa1f64779204cdef8fb5b576a3
Aviral000 <avimalik02@gmail.com>
2024-11-18 16:34:55 +0530

    removed the animation

--- Nov-18/assign2/src/components/UserList.tsx
+++ Nov-18/assign2/src/components/UserList.tsx
@@ -1,35 +1,10 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { UserDetail } from '../models/user.model'
 import axios from 'axios';
-import gsap from 'gsap';
 
 const UserList: React.FC = () => {
 const [users, setUsers] = useState<UserDetail[]>([]);
 const [selectedUsers, setSelectedUsers] = useState<UserDetail[]>([]);
-const selectRef = useRef<HTMLSelectElement>(null);
-const tableRef = useRef<HTMLTableElement>(null);
-
-useLayoutEffect(() => {
-    gsap.fromTo(selectRef.current, {
-        x: "+100vw",
-        opacity: 0
-    }, {
-        x: 0,
-        opacity: 1,
-        duration: 2,
-        ease: 'bounce.inOut'
-    });
-
-    gsap.fromTo(tableRef.current, {
-        y: "50vh",
-        opacity: 0
-    }, {
-        y: 0,
-        delay: 2,
-        opacity: 1,
-        duration: 2
-    });
-}, [])
 
 const getData = async () => {
     try {
@@ -80,7 +55,7 @@ const getUniqueCountries = () => {
   return (
     <div>
       <div>
-        <select onChange={handleChangeCountry} className='m-8 p-2 border-2 border-black' ref={selectRef}>
+        <select onChange={handleChangeCountry} className='m-8 p-2 border-2 border-black'>
             <option>Select a country</option>
             {getUniqueCountries().map((country, index) => (
                 <option key={`${country}-${index}`} value={country}>
@@ -90,7 +65,7 @@ const getUniqueCountries = () => {
         </select>
       </div>
       <div>
-        <table className='w-full text-center' ref={tableRef}>
+        <table className='w-full text-center'>
             <thead className='bg-orange-600 text-3xl font-light border-2 border-black'>
                 <tr>
                     <th>Customer Name</th>
