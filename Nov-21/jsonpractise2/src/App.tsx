2288cb26c6f751a95fc71de0e1d5bce089e4cd58
Aviral000 <avimalik02@gmail.com>
2024-11-21 12:11:53 +0530

    2nd example changes has been made as per the sir request. Practise file 2 for json server

---
+++ Nov-21/jsonpractise2/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeptsCrud from './components/DeptsCrud';
+
+function App() {
+  return (
+    <div>
+      <DeptsCrud />
+    </div>
+  );
+}
+
+export default App;
