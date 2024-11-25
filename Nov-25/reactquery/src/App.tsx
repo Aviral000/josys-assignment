48eeadd3b57fc0e649532cc016c503914ed8dad8
Aviral000 <avimalik02@gmail.com>
2024-11-25 13:04:49 +0530

    code snippet for react-hook-form

--- Nov-25/reactquery/src/App.tsx
+++ Nov-25/reactquery/src/App.tsx
@@ -14,3 +14,53 @@ function App() {
 }
 
 export default App;
+
+// import React from "react"
+// import { useForm, Resolver } from "react-hook-form"
+
+
+// type FormValues = {
+//   firstName: string
+//   lastName: string
+// }
+
+
+// const resolver: Resolver<FormValues> = async (values) => {
+//   return {
+//     values: values.firstName ? values : {},
+//     errors: !values.firstName
+//       ? {
+//           firstName: {
+//             type: "required",
+//             message: "This is required.",
+//           },
+//         }
+//       : {},
+//   }
+// }
+
+
+// const App: React.FC = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<FormValues>({ resolver })
+//   const onSubmit = handleSubmit((data) => console.log(data))
+
+
+//   return (
+//     <form onSubmit={onSubmit}>
+//       <input {...register("firstName")} placeholder="Bill" />
+//       {errors?.firstName && <p>{errors.firstName.message}</p>}
+
+
+//       <input {...register("lastName")} placeholder="Luo" />
+
+
+//       <input type="submit" />
+//     </form>
+//   )
+// }
+
+// export default App;
